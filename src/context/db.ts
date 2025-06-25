2b03ef3ebc5a44c9eeb9e8c4d3658cf1d4375085
David Ugbero <98487635+david-iov@users.noreply.github.com>
2025-06-25 16:33:25 +0100

    feat: add gha workflow for state sync cronjob on all envs (#2)
    * feat: add gha workflow for cr-qa state sync cronjob
    
    * feat: trigger workflow
    
    * fix`; debugging
    
    * feat: build the docker and run state sync
    
    * feat: add deploy workflow to ECS
    
    * feat: trigger build
    
    * feat: trigger build
    
    * feat: trigger build
    
    * build(Dockerfile): expose port 3000
    
    * refactor: do not expose ports
    
    * refactor: test
    
    * feat: download aws rds ca cert and use for db conn
    
    * refactor: restart
    
    * feat: update ecs resources
    
    * feat: implement workflow deploy actions for all envs
    
    * feat: add workflow for mainnet
    
    * feat: update workflow environment
    
    * fix: set the deployment triggers
    
    * fix: develop deployment
    
    * build: release candidate mainnet
    
    ---------
    
    Co-authored-by: Francisco Tobar <francisco.tobar@iovlabs.org>
    Co-authored-by: Antonio <antonio@iovlabs.org>

--- src/context/db.ts
+++ src/context/db.ts
@@ -1,5 +1,7 @@
 import { Database } from '../config/types';
 import knex, { Knex } from 'knex';
+import * as fs from 'fs';
+
 interface DatabaseContext {
     db: Knex;
     batchSize: number;
@@ -10,10 +12,19 @@ interface DatabaseContext {
 // Factory function to create a database context
 const createDatabaseContext = (database: Database): DatabaseContext => {
     const { connectionString, ...rest } = database;
+    
+    // Check if SSL certificate exists and configure SSL accordingly
+    const certPath = '/app/rds-ca-cert.pem';
+    const sslConfig = fs.existsSync(certPath) ? {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(certPath).toString(),
+    } : false;
+    
     const db = knex({
         client: 'pg',
         connection: {
             connectionString,
+            ssl: sslConfig,
         },
     });
 
