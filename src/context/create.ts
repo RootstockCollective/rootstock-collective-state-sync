cbbf726dd3481704c6dcaed2aa9e6fd82817df15
Sleyter Sandoval <sleyter.sandoval@iovlabs.org>
2025-07-09 10:08:58 -0500

    DAO-1372 add proposals entities

--- src/context/create.ts
+++ src/context/create.ts
@@ -1,15 +1,23 @@
 import { Config } from "../config/types";
 import { createSchemaContext } from "./schema";
 import { createDatabaseContext } from "./db";
-import { createTheGraphContext } from "./subgraphProvider";
+import { createSubgraphContext, GraphQlContext } from "./subgraphProvider";
 import { AppContext } from "./types";
 
 
-const createContexts = (config: Config): AppContext => ({
-    schema: createSchemaContext(config.entities),
-    dbContext: createDatabaseContext(config.database),
-    graphqlContext: createTheGraphContext(config.subgraphProvider),
-    config
-});
+const createContexts = (config: Config): AppContext => {
+    const graphqlContexts: Record<string, GraphQlContext> = {};
+    
+    for (const provider of config.subgraphProviders) {
+        graphqlContexts[provider.name] = createSubgraphContext(provider);
+    }
+
+    return {
+        schema: createSchemaContext(config.entities),
+        dbContext: createDatabaseContext(config.database),
+        graphqlContexts,
+        config
+    };
+};
 
 export { createContexts }
