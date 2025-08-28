392e47e1368b1771e886209fa1c62c0192199678
franciscotobar <100875069+franciscotobar@users.noreply.github.com>
2025-08-27 19:11:31 -0600

    TOK-776/daily-allocation (#52)
    * feat: daily allocation
    
    * refactor: BigInt numeric

--- src/handlers/types.ts
+++ src/handlers/types.ts
@@ -1,25 +1,50 @@
-type ColumnType = 'Boolean' | 'BigInt' | 'Bytes' | 'String';
+import { Knex } from 'knex';
+
+type ColumnType = 'Boolean' | 'BigInt' | 'Bytes' | 'String' | 'Integer';
 type ArrayColumnType = [ColumnType];
 
-const columnTypeMap: Record<ColumnType, string> = {
-    Boolean: 'BOOLEAN',
-    BigInt: 'TEXT',
-    Bytes: 'BYTEA',
-    String: 'TEXT'
+interface ColumnTypeConfig {
+    sqlType: string;
+    knexHandler: (table: Knex.TableBuilder, name: string) => Knex.ColumnBuilder;
+}
+
+const columnTypeConfigs: Record<ColumnType, ColumnTypeConfig> = {
+    Boolean: {
+        sqlType: 'BOOLEAN',
+        knexHandler: (table, name) => table.boolean(name).notNullable()
+    },
+    BigInt: {
+        sqlType: 'NUMERIC',
+        knexHandler: (table, name) => table.decimal(name, 78, 0).notNullable()
+    },
+    Bytes: {
+        sqlType: 'BYTEA',
+        knexHandler: (table, name) => table.binary(name).notNullable()
+    },
+    String: {
+        sqlType: 'TEXT',
+        knexHandler: (table, name) => table.text(name).notNullable()
+    },
+    Integer: {
+        sqlType: 'INTEGER',
+        knexHandler: (table, name) => table.integer(name).notNullable()
+    }
 } as const;
 
 const isColumnType = (type: string): type is ColumnType =>
-    Object.keys(columnTypeMap).includes(type);
+    Object.keys(columnTypeConfigs).includes(type);
 
 const isArrayColumnType = (type: string | string[]): type is ArrayColumnType => {
     return Array.isArray(type) &&
         type.length === 1 &&
         typeof type[0] === "string" &&
-        ["String", "Boolean", "BigInt", "Bytes"].includes(type[0]);
+        isColumnType(type[0]);
 }
+
+
 
 type EntityRecord = unknown & { id: string };
 type EntityDataCollection = Record<string, EntityRecord[]>;
 
-export type { ColumnType, ArrayColumnType, EntityDataCollection, EntityRecord }
-export { columnTypeMap, isColumnType, isArrayColumnType }
+export type { ColumnType, ArrayColumnType, EntityDataCollection, EntityRecord, ColumnTypeConfig }
+export { columnTypeConfigs, isColumnType, isArrayColumnType }
