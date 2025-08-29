a951fd6ffb4fc4da70c56577a6bb5794e59cb227
Antonio <antonio@iovlabs.org>
2025-08-29 17:06:39 +0200

    chore: remove unused code

--- src/watchers/strategies/reorgCleanupStrategy.ts
+++ src/watchers/strategies/reorgCleanupStrategy.ts
@@ -1,40 +1,16 @@
 import { info } from 'loglevel';
 import { Hex, PublicClient } from 'viem';
-import { DatabaseContext, PUBLIC_SCHEMA } from '../../context/db';
+import { PUBLIC_SCHEMA } from '../../context/db';
 import { AppContext } from '../../context/types';
 import { createContextWithSchema } from '../../context/create';
-import { BlockChangeLog, ChangeStrategy } from './types';
+import { ChangeStrategy } from './types';
 import { getLastProcessedBlock } from './utils';
 import { createSchema, switchSchema } from '../../handlers/schema';
 import { createDb } from '../../handlers/dbCreator';
 import { syncEntities } from '../../handlers/subgraphSyncer';
 
-const getBlockFromNode = (client: PublicClient, blockNumber: bigint) => {
-  return client.getBlock({ blockNumber });
-}
-
-const areHashesEqual = (a: Hex, b: Hex): boolean => {
-  return a === b;
-}
-
 const convertDbIdToHash = (id: string): Hex => {
   return Buffer.from(id, 'hex').toString('utf-8') as Hex;
-}
-
-const fetchBatchByBlockNumberDesc = async (
-  db: DatabaseContext['db'],
-  fromBlockExclusive?: bigint,
-  limit = 1000
-): Promise<BlockChangeLog[]> => {
-  let query = db<BlockChangeLog>('BlockChangeLog')
-    .orderBy('blockNumber', 'desc')
-    .limit(limit);
-
-  if (fromBlockExclusive !== undefined) {
-    query = query.where('blockNumber', '<', fromBlockExclusive);
-  }
-
-  return query;
 }
 
 const NEW_SCHEMA = 'tmp_public';
